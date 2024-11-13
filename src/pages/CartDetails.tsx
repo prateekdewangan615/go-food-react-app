893f8b26d9bd9ac08596acabeb262a99c2bd7b3d
Aakash <aakashv72003@gmail.com>
2024-11-13 23:00:43 +0530

    Cart Check Out Updated

--- src/pages/CartDetails.tsx
+++ src/pages/CartDetails.tsx
@@ -65,26 +65,48 @@ const CartDetails = () => {
               ))}
             </tbody>
           </Table>
+          <div className="d-flex justify-content-center">
           <div
             className="card justify-content-end"
-            style={{ width: "18rem", justifyContent: "right" }}
+            style={{ width: "18rem", justifyContent: "center" }}
           >
-            <div className="card">
-              <div className="card-body d-grid gap-2 justify-content-center align-items-center">
-                {" "}
-                {/*Using Grid*/}
-                <div className="d-flex justify-content-center">
-                  <div className="card-text">
-                    <p>Subtotal : ₹{total}</p>
-                    <p>Delivery Fee : ₹{deliveryFee}</p>
-                    <p>
-                      <b>Total: ₹{getTotalAmount()}</b>
-                    </p>
-                  </div>
+            <div
+              className="card "
+              style={{
+                background:
+                  "linear-gradient(to bottom right, #ff9966, #ffcc99)",
+              }}
+            >
+              <div className="card-body">
+                <table className="table ">
+                  <tbody>
+                    <tr>
+                      <td>Subtotal</td>
+                      <td className="text-end">₹{total}</td>{" "}
+                    </tr>
+                    <tr>
+                      <td>Delivery Fee</td>
+                      <td className="text-end">₹{deliveryFee}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <b>Total</b>
+                      </td>
+                      <td className="text-end">
+                        <b>₹{getTotalAmount()}</b>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div className="d-grid gap-2">
+                  <button onClick={handleCheckout} className="btn btn-primary">
+                    Proceed To Checkout
+                  </button>
                 </div>
-                <button onClick={handleCheckout}>Proceed To Checkout</button>
               </div>
             </div>
+          </div>
+
           </div>
         </>
       ) : (
